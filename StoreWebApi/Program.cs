1f9c592acb77ee5ec5fc9e90c26a3aa021f2ae87
kenikor <romanluev0@gmail.com>
2025-06-20 10:48:50 +0300

    Update Program.cs

--- StoreWebApi/Program.cs
+++ StoreWebApi/Program.cs
@@ -1,23 +1,21 @@
-using StoreLibrary.Data;
-
-var builder = WebApplication.CreateBuilder(args);
-
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddDbContext<AppDbContext>();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+<Window x:Class="WpfTextEditor.MainWindow"
+        xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+        Title="TextEditor" Height="450" Width="800"
+        KeyDown="Window_KeyDown">
+    <DockPanel>
+        <Menu DockPanel.Dock="Top">
+            <MenuItem Header="_File">
+                <MenuItem Header="Open" Click="OpenFile_Click" InputGestureText="Ctrl+O"/>
+                <MenuItem Header="Save" Click="SaveFile_Click" InputGestureText="Ctrl+S"/>
+                <Separator/>
+                <MenuItem Header="Set Default Folder" Click="SetDefaultFolder_Click"/>
+                <Separator/>
+                <MenuItem Header="Exit" Click="Exit_Click"/>
+            </MenuItem>
+        </Menu>
+        <TextBox Name="TextEditorBox" AcceptsReturn="True" AcceptsTab="True"
+                 VerticalScrollBarVisibility="Auto" HorizontalScrollBarVisibility="Auto"
+                 FontSize="14" TextWrapping="Wrap"/>
+    </DockPanel>
+</Window>
